107c7033107599e59ccb1506390665e0d49978f1
Kazuki Nishikawa <kz.nishikawa@gmail.com>
2020-09-14 23:11:27 +0900

    typing

--- src/index.ts
+++ src/index.ts
@@ -1,46 +1,43 @@
-// Result
-//   commandName: string
-//   createdAt: Date
-//   files: FileList
-// FileList
-//   files: [SourceFile]
-// SourceFile
-//   filename: string
-//   coverageData: CoverageData
-// CoverageData
-//   lines: [number | null]
-//   branches: { string => { string => number } }
+export class Result {
+  commandName: string;
+  timestamp: number;
+  files: FileList;
 
-class Result {
-  constructor(commandName, timestamp, coverage) {
+  constructor(commandName: string, timestamp: number, coverage: any) {
     this.commandName = commandName
     this.timestamp = timestamp
     this.files = FileList.parse(coverage)
   }
 
-  static parse(rawResult) {
-    const commandName = Object.keys(rawResult)[0];
-    const { coverage, timestamp } = rawResult[commandName]
+  static parse(rawResult: any): Result {
+    const commandName = Object.keys(rawResult)[0] as string;
+    const { coverage, timestamp } = rawResult[commandName] as { coverage: any, timestamp: number }
     return new Result(commandName, timestamp, coverage)
   }
 }
 
-class FileList {
-  constructor(files) {
+type CoverageStaticticsByType = {
+  line: CoverageStatistics
+}
+
+export class FileList {
+  files: SourceFile[];
+
+  constructor(files: SourceFile[]) {
     this.files = files
   }
 
-  coverageStatistics() {
+  coverageStatistics(): CoverageStaticticsByType  {
     return {
       line: CoverageStatistics.merge(this.files.map(f => f.coverageStatistics().line))
     }
   }
 
-  get length() {
+  get length(): number {
     return this.files.length
   }
 
-  static parse(coverage) {
+  static parse(coverage: { [key: string]: CoverageData }): FileList {
     const files = Object.entries(coverage).map(([filename, coverageData]) => {
       return new SourceFile(filename, coverageData)
     })
@@ -48,28 +45,36 @@ class FileList {
   }
 }
 
-class SourceFile {
-  constructor(filename, coverageData) {
+type CoverageData = {
+  lines: (number | null)[]
+}
+
+export class SourceFile {
+  filename: string;
+  coverageData: CoverageData;
+  _lines: Line[] | undefined
+
+  constructor(filename: string, coverageData: CoverageData) {
     this.filename = filename
     this.coverageData = coverageData
   }
 
-  lines() {
+  lines(): Line[] {
     if (!this._lines) {
-      this._lines = this.coverageData['lines'].map((c, i) => new Line(i + 1, c))
+      this._lines = this.coverageData.lines.map((c, i) => new Line(i + 1, c))
     }
     return this._lines
   }
 
-  coveredLines() {
+  coveredLines(): Line[] {
     return this.lines().filter(l => l.isCovered)
   }
 
-  missedLines() {
+  missedLines(): Line[] {
     return this.lines().filter(l => l.isMissed)
   }
 
-  coverageStatistics() {
+  coverageStatistics(): CoverageStaticticsByType {
     return {
       line: new CoverageStatistics(this.coveredLines().length, this.missedLines().length)
     }
@@ -77,24 +82,27 @@ class SourceFile {
 }
 
 class CoverageStatistics {
-  constructor(covered, missed) {
+  covered: number;
+  missed: number;
+
+  constructor(covered: number, missed: number) {
     this.covered = covered
     this.missed = missed
   }
 
-  get total() {
+  get total(): number {
     return this.covered + this.missed
   }
 
-  get ratio() {
+  get ratio(): number {
     return this.total === 0 ? 1 : this.covered / this.total
   }
 
-  get percent() {
+  get percent(): number {
     return this.ratio * 100
   }
 
-  static merge(coverageStatistics) {
+  static merge(coverageStatistics: CoverageStatistics[]): CoverageStatistics {
     const [covered, missed] = coverageStatistics.reduce(([c, m], stats) => {
       return [c + stats.covered, m + stats.missed]
     }, [0, 0])
@@ -104,26 +112,23 @@ class CoverageStatistics {
 }
 
 class Line {
-  constructor(lineNumber, coverage) {
+  lineNumber: number;
+  coverage: number | null;
+
+  constructor(lineNumber: number, coverage: number | null) {
     this.lineNumber = lineNumber
     this.coverage = coverage
   }
 
-  get isCovered() {
-    return this.coverage > 0
+  get isCovered(): boolean {
+    return this.coverage !== null && this.coverage > 0
   }
 
-  get isMissed() {
-    return this.coverage === 0
+  get isMissed(): boolean {
+    return this.coverage !== null && this.coverage === 0
   }
 
-  get isNever() {
+  get isNever(): boolean {
     return this.coverage === null
   }
-}
-
-module.exports = {
-  FileList,
-  SourceFile,
-  Result,
 }
